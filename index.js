a3da139d40ab062204d89d74f51c91d08ae71a7a
adrianhumphrey111 <adrianhumphrey374@gmail.com>
2023-11-21 16:07:01 -0600

    Add redirect url to conversation.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const yaml = require('js-yaml');
 const axios = require('axios')
 const app = express();
+const cheerio = require('cheerio')
 
 const CLIENT_ID = '86evj6mojpyxvv';
 const CLIENT_SECRET = 'XLbTDKBqLYHo6n51';
@@ -14,12 +15,40 @@ const REDIRECT_URI = 'http://localhost:3001/auth/linkedin/callback';
 app.use(cors());
 const port = 3001
 
+const accessTokeMapByConversation = new Map()
+
+async function scrapeLinkedInProfile(url) {
+  try {
+    // Fetch the HTML of the page
+    const response = await axios.get(url);
+    console.log({response})
+    const $ = cheerio.load(response.data);
+
+    // Parse and extract data as needed
+    // For example, extracting the bio:
+    const bio = $('#bio-id').text();
+
+    // Create a JSON object with the extracted data
+    const profileData = {
+      bio: bio,
+      // Add other fields as needed
+    };
+
+    // Return or process the JSON data
+    return profileData;
+  } catch (error) {
+    console.error('Error scraping LinkedIn:', error);
+  }
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/todos', async (req, res) => {
   console.log("Request Headers: ", req.headers);
+  
     const todos = [
         "This is the first todo",
         "This is the second todo"
@@ -27,18 +56,47 @@ app.get('/todos', async (req, res) => {
     res.json({todos})
 })
 
+app.get('/userProfile', async (req, res) => {
+  const url = req.query.url
+  const conversationId = req.headers['openai-conversation-id'] || ''
+  const access_token = accessTokeMapByConversation.get(conversationId)
+  try {
+    // Make a request to get my own profile
+    const headers = {
+      Authorization: `Bearer ${access_token}`,
+      'X-RestLi-Protocol-Version': '2.0.0' // Required by LinkedIn for certain API calls
+    };
+    
+    // Making the request to retrieve the current member's profile
+    axios.get('https://api.linkedin.com/v2/people/(id:{profile ID})?projection=(id,firstName,lastName)', { headers: headers })
+      .then(response => {
+        // Handle the response data
+        console.log(response.data);
+      })
+      .catch(error => {
+        // Handle any errors
+        console.error('Error making API request:', error);
+      });
+  }catch(e){
+    console.log(e)
+    res.status(200).send()
+  }  
+})
+
 
 // Route to start the OAuth flow
 app.get('/auth/linkedin', (req, res) => {
-  const linkedInAuthURL = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=openid%20profile%20w_member_social%20email`;
-  res.redirect(linkedInAuthURL);
+  // Every conversation needs to have an access token
+  const conversationId = req.headers['openai-conversation-id'] || ''
+  const linkedInAuthURL = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&state=${conversationId}&scope=openid%20profile%20w_member_social%20email`;
+  res.json({redirectUrl: linkedInAuthURL});
 });
 
 
 // Callback route
 app.get('/auth/linkedin/callback', async (req, res) => {
     const code = req.query.code;
-    console.log({code})
+    const conversationId = req.query.state
     try {
         const response = await axios.post('https://www.linkedin.com/oauth/v2/accessToken', null, {
             params: {
@@ -52,9 +110,14 @@ app.get('/auth/linkedin/callback', async (req, res) => {
                 'Content-Type': 'application/x-www-form-urlencoded'
             }
         });
+
+        const meProfileResponse = await axios.get('https://api.linkedin.com/v2/me', { headers: headers })
+        const personId = response.data.id
+
+        // get profile with `https://api.linkedin.com/v2/people/(id:${personID})`
+
         const accessToken = response.data.access_token;
-        console.log({accessToken})
-        // Use the access token as needed
+        accessTokeMapByConversation.set(conversationId, accessToken)
         res.send('Access Token Obtained!');
     } catch (error) {
         console.error('Error obtaining access token:', error);
